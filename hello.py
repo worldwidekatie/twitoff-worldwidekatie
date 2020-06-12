035f3af9b9097118323f41f254bd175ffce261ff
worldwidekatie <katie.evankodouglas@gmail.com>
2020-06-12 14:56:46 -0600

    hello world

---
+++ hello.py
@@ -0,0 +1,13 @@
+# hello.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x = 2+2
+    return f"Hello World! {x}"
+
+@app.route("/about")
+def about():
+    return "About me"
