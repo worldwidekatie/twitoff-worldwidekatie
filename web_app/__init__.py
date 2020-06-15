df157797372902c2e1522aa602950cefe992c952
worldwidekatie <katie.evankodouglas@gmail.com>
2020-06-15 12:57:30 -0600

    books mvp

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,14 +1,21 @@
-# web_app/__init__.py
-
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.tweet_routes import tweet_routes
+from web_app.routes.book_routes import book_routes
+
+DATABASE_URL = "sqlite:///twitoff_15_development.db" # using relative filepath
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
-    #app.register_blueprint(tweet_routes)
+    app.register_blueprint(book_routes)
     return app
 
 if __name__ == "__main__":
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,12 +1,12 @@
-# web_app/routes/home_routes.py
-
+from flask import Flask
 from flask import Blueprint
 
 home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 def index():
-    return "Welcome to Katie's Twitoff Assignment."
+    #x = 2 + 2
+    return f"Welcome to Katie's twitoff assignment"
 
 @home_routes.route("/about")
 def about():
