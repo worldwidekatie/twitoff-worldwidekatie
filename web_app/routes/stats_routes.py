69e254b57c682cf630d29efae40ab348a3fbcbdf
worldwidekatie <katie.evankodouglas@gmail.com>
2020-06-17 12:29:30 -0600

    got predictive stuff working

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -2,10 +2,10 @@
 
 from flask import Blueprint, request, jsonify, render_template
 
-#from sklearn.linear_model import LogisticRegression # for example
+from sklearn.linear_model import LogisticRegression
 
-#from web_app.models import User, Tweet
-#from web_app.services.basilica_service import basilica_api_client
+from web_app.models import User, Tweet
+from web_app.services.basilica_service import connection as basilica_api_client
 
 stats_routes = Blueprint("stats_routes", __name__)
 
@@ -19,16 +19,47 @@ def predict():
 
     print("-----------------")
     print("FETCHING TWEETS FROM THE DATABASE...")
+    # h/t: https://flask-sqlalchemy.palletsprojects.com/en/2.x/queries/
+    # get the embeddings (from the database)
+    user_a = User.query.filter_by(screen_name=screen_name_a).first()
+    user_b = User.query.filter_by(screen_name=screen_name_b).first()
+    user_a_tweets = user_a.tweets
+    user_b_tweets = user_b.tweets
+    print("FETCHED TWEETS", len(user_a_tweets), len(user_b_tweets))
 
     print("-----------------")
     print("TRAINING THE MODEL...")
+    # X values / inputs: embeddings
+    # Y values / labels: screen_names
+
+    classifier = LogisticRegression()
+
+    embeddings = []
+    labels = []
+
+    for tweet in user_a_tweets:
+        embeddings.append(tweet.embedding)
+        labels.append(screen_name_a) # tweet.user.screen_name
+
+    for tweet in user_b_tweets:
+        embeddings.append(tweet.embedding)
+        labels.append(screen_name_b) # tweet.user.screen_name
+
+    classifier.fit(embeddings, labels)
 
     print("-----------------")
     print("MAKING A PREDICTION...")
+    #example_embed_a = user_a_tweets[3].embedding
+    #example_embed_b = user_b_tweets[3].embedding
+    #result = classifier.predict([example_embed_a, example_embed_b])
+
+    embedding = basilica_api_client.embed_sentence(tweet_text, model="twitter")
+
+    result = classifier.predict([embedding])
 
     return render_template("prediction_results.html",
         screen_name_a=screen_name_a,
         screen_name_b=screen_name_b,
         tweet_text=tweet_text,
-        screen_name_most_likely= "TODO" #result[0]
+        screen_name_most_likely=result[0]
     )
