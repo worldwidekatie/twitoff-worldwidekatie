82d976a295388bc9b88d3d831a2ad2088c3b59b3
worldwidekatie <katie.evankodouglas@gmail.com>
2020-06-18 11:41:09 -0600

    update

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,7 @@
 
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
@@ -7,11 +9,15 @@ from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URL = "sqlite:///twitoff_15_development.db" # using relative filepath
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+#SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
 
+    #app.config["SECRET_KEY"] = SECRET_KEY
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
